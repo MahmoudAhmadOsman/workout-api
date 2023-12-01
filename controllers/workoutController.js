63c6c232a17873de0bf12de9d8d5d32c93eef74e
Mahmoud Osman <mahmoudosman.som@gmail.com>
2023-11-30 21:39:14 -0600

    added error handling array message

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -1,5 +1,5 @@
-const Workout = require("../models/workoutModel");
 const mongoose = require("mongoose");
+const Workout = require("../models/workoutModel");
 
 //get all workouts
 
@@ -16,6 +16,25 @@ const getWorkouts = async (req, res) => {
 const createWorkout = async (req, res) => {
 	const { title, load, reps } = req.body;
 
+	//check error
+
+	let emptyFields = [];
+	if (!title) {
+		emptyFields.push("title");
+	}
+	if (!load) {
+		emptyFields.push("load");
+	}
+	if (!reps) {
+		emptyFields.push("reps");
+	}
+
+	if (emptyFields.length > 0) {
+		return res
+			.status(400)
+			.json({ error: "Please fill in all the fields", emptyFields });
+	}
+
 	try {
 		const workout = await Workout.create({ title, load, reps });
 		res.status(200).json(workout);
@@ -24,7 +43,7 @@ const createWorkout = async (req, res) => {
 	}
 };
 
-//Get workout by id
+//Get single workout by its id
 const getWorkout = async (req, res) => {
 	const { id } = req.params;
 
@@ -41,20 +60,21 @@ const getWorkout = async (req, res) => {
 };
 
 //delete a workout
+
 const deleteWorkout = async (req, res) => {
 	const { id } = req.params;
 
 	if (!mongoose.Types.ObjectId.isValid(id)) {
-		return res.status(404).json({ error: "No such workout" });
+		return res.status(400).json({ error: "No such workout" });
 	}
+
 	const workout = await Workout.findOneAndDelete({ _id: id });
 
 	if (!workout) {
 		return res.status(400).json({ error: "No such workout" });
 	}
-
-	// res.status(200).json(workout);
-	res.status(200).json("Workout has been deleted!");
+	// res.status(200).json("Workout has been deleted!");
+	res.status(200).json(workout);
 };
 
 // update a workout
