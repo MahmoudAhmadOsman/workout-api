bb461784440e854709e92ff8228118de0c210108
Mahmoud Osman <mahmoudosman.som@gmail.com>
2023-12-09 23:57:50 -0600

    added protected routes in the App.js component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import "./App.css";
 import Navigation from "./components/Navigation";
@@ -6,16 +6,30 @@ import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import NotFound from "./utils/NotFound";
+import { useAuthContext } from "./hooks/useAuthContext";
+
 function App() {
+	const { user } = useAuthContext(); // get logged user
+
 	return (
 		<div className="home-paage">
 			<BrowserRouter>
 				<Navigation />
 				<div className="pages">
 					<Routes>
-						<Route path="/signup" element={<Signup />} />
-						<Route path="/login" element={<Login />} />
-						<Route path="/" element={<Home />} />
+						{" "}
+						<Route
+							path="/"
+							element={user ? <Home /> : <Navigate to="/login" />}
+						/>
+						<Route
+							path="/login"
+							element={!user ? <Login /> : <Navigate to="/" />}
+						/>
+						<Route
+							path="/signup"
+							element={!user ? <Signup /> : <Navigate to="/" />}
+						/>
 						<Route path="*" element={<NotFound />} />
 					</Routes>
 				</div>
