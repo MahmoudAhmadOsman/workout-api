d8d68ce352704ebbf84bc12a6c1a6b95b7475a16
Mahmoud Osman <34916796+MahmoudAhmadOsman@users.noreply.github.com>
2023-12-16 18:43:26 -0600

    Update Profile.js

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -3,12 +3,12 @@ import { useAuthContext } from "../hooks/useAuthContext"; // to get the logged u
 
 const Profile = () => {
  // Sample user data
-  const user = {
-    firstName: 'John',
-    lastName: 'Doe',
-    email: 'johndoe@example.com',
-    imageURL: 'https://via.placeholder.com/150', // Placeholder image URL
-  };
+  // const user = {
+  //   firstName: 'John',
+  //   lastName: 'Doe',
+  //   email: 'johndoe@example.com',
+  //   imageURL: 'https://via.placeholder.com/150', // Placeholder image URL
+  // };
 
   return (
     <div className="container mt-4">
@@ -19,24 +19,23 @@ const Profile = () => {
         <div className="card-body">
           <div className="text-center">
             <img
-              src={user.imageURL}
+              src="https://via.placeholder.com/150"
               alt="User Profile"
               className="rounded-circle mb-3"
               style={{ width: '150px', height: '150px' }}
             />
           </div>
           <p className="card-text">
-            <strong>First Name:</strong> {user.firstName}
+            <strong>First Name:</strong> John 
           </p>
           <p className="card-text">
-            <strong>Last Name:</strong> {user.lastName}
+            <strong>Last Name:</strong> Doe
           </p>
           <p className="card-text">
             <strong>Email:</strong> 
              {user && (
-													<Link className="nav-link">{user.email}</Link>
-				
-											)} 
+		<Link className="nav-link">{user.email}</Link>	
+		)} 
           </p>
         </div>
       </div>
