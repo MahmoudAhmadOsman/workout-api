532e49570f835b09f4ad30fb8ca62ab2ff58c7d6
Mahmoud Osman <34916796+MahmoudAhmadOsman@users.noreply.github.com>
2023-12-16 19:14:15 -0600

    Update Profile.js

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useAuthContext } from "../hooks/useAuthContext"; // to get the logged user information
 
 const Profile = () => {
